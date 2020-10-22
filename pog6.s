a168794e64f84834a07ac554465f8eab3332fac4
ExStefenGR <stt0242@my.londonmet.ac.uk>
2020-10-22 22:33:02 +0100

    finding a way to add animation using existing terms

---
+++ pog6.s
@@ -0,0 +1,29 @@
+.arm
+.text
+.global main
+main:
+	mov r0, #0x4000000
+	mov r1, #0x400
+	add r1, r1, #3
+	str r1, [r0]
+
+	mov r0, #0x6000000
+	mov r1, #0x0F @red colour
+
+	@Beginning of loop
+	mov r2, #0xF @F is for a row of Pixels Horizontally
+	add r2,  #0x1A
+Pixels1:
+	strh r1, [r0], #0x1
+	subs r2, r2, #0x1
+	bne Pixels1 @end of Pixels1
+
+
+	mov r1, #0xFF00 @loop2 colour
+	add r1, #0x2A
+Pixels2:
+	strh r1, [r0], #0x2
+	subs r2, r2, #0x1A
+
+infin:
+	b infin
